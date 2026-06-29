5f7835ae59f49c400378b9bdfa2e29607fd8f860
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2026-06-29 22:15:33 +0300

    main

--- app/services/users.ts
+++ app/services/users.ts
@@ -3,7 +3,7 @@ import { db } from "@/db";
 import { users } from "@/db/schema";
 
 export const getUsers = async () => {
-  return db.query.users.findMany()
+  return db.select().from(users)
 }
 
 export const getUserByUsername = async (username: string) => {
