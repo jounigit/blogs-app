77fa8b01aabc857ea4615024e2a8e52ad1542488
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2026-07-01 09:36:11 +0300

    15. Validations in user registration

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -4,11 +4,43 @@ import { redirect } from "next/navigation"
 import bcrypt from "bcryptjs"
 import { db } from "@/db"
 import { users } from "@/db/schema"
+import { eq } from "drizzle-orm"
 
-export const registerUser = async (formData: FormData) => {
+export const registerUser = async (
+  prevState: { errors: Record<string, string>; values: Record<string, string> },
+  formData: FormData
+) => {
   const username = (formData.get("username") as string)?.trim()
   const name = (formData.get("name") as string)?.trim()
   const password = formData.get("password") as string
+  const confirmPassword = formData.get("confirmPassword") as string
+
+  if (password !== confirmPassword) {
+    return { errors: { confirmPassword: "Passwords do not match" }, values: { username, name } }
+  }
+  const errors: Record<string, string> = {}
+
+  if (!username || username.length < 4) {
+    errors.username = "Username must be at least 4 characters long"
+  }
+
+  if (!name || name.length < 4) {
+    errors.name = "Name must be at least 4 characters long"
+  }
+
+  if (!password || password.length < 6) {
+    errors.password = "Password must be at least 6 characters long"
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors, values: { username, name } }
+  }
+
+  const existingUser = await db.select().from(users).where(eq(users.username, username))
+
+  if (existingUser) {
+    return { errors: { username: "Username already exists" }, values: { username, name } }
+  }
 
   const passwordHash = await bcrypt.hash(password, 10)
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,34 +1,54 @@
+"use client"
+
 import { registerUser } from "@/app/actions/users"
+import { useActionState } from "react"
 
 export default function RegisterPage() {
+  const [state, formAction] = useActionState(registerUser, { errors: {}, values: { username: "", name: "" } })
   return (
     <div className="m-4">
       <h2 className="text-2xl font-bold">Register</h2>
-      <form action={registerUser}>
+      <form action={formAction}>
         <div>
-          <label>
-            Username
-            <input type="text" name="username" required 
+          <label>Username</label>
+            <input 
+              type="text" 
+              name="username" 
+              defaultValue={state.values?.username}
+              required 
+              className="border border-gray-300 rounded px-2 py-1 w-full"
+            />
+          {state.errors?.username && <p className="text-red-600 text-sm">{state.errors?.username}</p>}
+        </div>
+
+        <div>
+          <label>Name</label>
+            <input 
+             type="text" 
+             name="name" 
+             defaultValue={state.values?.name} 
+             required 
             className="border border-gray-300 rounded px-2 py-1 w-full"
             />
-          </label>
+          {state.errors?.name && <p className="text-red-600 text-sm">{state.errors?.name}</p>}
         </div>
+
         <div>
-          <label>
-            Name
-            <input type="text" name="name" required 
-            className="border border-gray-300 rounded px-2 py-1 w-full"
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
+          <label>Password</label>
             <input type="password" name="password" required 
             className="border border-gray-300 rounded px-2 py-1 w-full"
             />
-          </label>
+          {state.errors?.password && <p className="text-red-600 text-sm">{state.errors?.password}</p>}
         </div>
+
+        <div>
+          <label>Confirm Password</label>
+            <input type="password" name="confirmPassword" required 
+            className="border border-gray-300 rounded px-2 py-1 w-full"
+            />
+          {state.errors?.confirmPassword && <p className="text-red-600 text-sm">{state.errors?.confirmPassword}</p>}
+        </div>
+        
         <button type="submit" 
         className="bg-blue-500 text-white px-4 py-2 rounded mt-4 hover:bg-blue-600">
           Register
